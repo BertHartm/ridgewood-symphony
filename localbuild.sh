56a4ca6024595e90117b2af58cf346ec314b7611
Bert Hartmann <BertHartm@gmail.com>
2019-07-02 20:28:10 -0400

    add gem caching to the build process for dramatic speed improvements

--- localbuild.sh
+++ localbuild.sh
@@ -1,1 +1,6 @@
-docker run --rm --volume="$PWD:/srv/jekyll" -it -p 4000:4000 jekyll/jekyll:3.8 jekyll serve
+#!/bin/bash
+set -euo pipefail
+set +x
+
+docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -it -p 4000:4000 jekyll/jekyll:3.8 bundle install
+docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -it -p 4000:4000 jekyll/jekyll:3.8 jekyll serve
