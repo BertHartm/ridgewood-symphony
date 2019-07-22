039454c15b0676e50184092d6b7c70bdf997280c
Bert Hartmann <BertHartm@gmail.com>
2019-07-22 17:54:33 -0400

    fix (hopefully) the 0.0.0.0 problem on windows

--- localbuild.sh
+++ localbuild.sh
@@ -2,5 +2,12 @@
 set -euo pipefail
 set +x
 
-docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -it -p 4000:4000 jekyll/jekyll:3.8 bundle install
-docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -it -p 4000:4000 jekyll/jekyll:3.8 jekyll serve
+# use "JEKYLL_ENV=asdf" because we can't be prod or dev
+# prod requires github auth tokens
+# dev sets the host to 0.0.0.0, and windows doesn't treat http://0.0.0.0:4000 as localhost:4000
+docker run --rm -e JEKYLL_ENV=asdf --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -it -p 4000:4000 \
+  jekyll/jekyll:3.8 \
+  bundle update
+docker run --rm -e JEKYLL_ENV=asdf --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -it -p 4000:4000 \
+  jekyll/jekyll:3.8 \
+  jekyll serve --port 4000
